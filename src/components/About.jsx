2a67ea874021c6f4a5fb8f0ae1b8acd6ec0938f7
kashaan_mahmood <kashaanmahmood@gmail.com>
2023-09-21 09:09:40 +0500

    edited

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,22 +4,23 @@ import { useGlobalContext } from '../context';
 import Modal from './Modal';
 
 export default function About() {
-  const { light, isModalOpen, openModal, isSubmitted, clicked, setClicked } = useGlobalContext();
+  const { light, isModalOpen, openModal, isSubmitted, clicked, setClicked } =
+    useGlobalContext();
   const click = clicked === 1;
   const freshModal = isModalOpen && click;
   return (
     <section className='about' aria-label='About Me'>
       <article className='about-intro-wrapper'>
         <div className='about-intro'>
-          <h3 className='about-intro-h3 about-intro-urdu' dir='rtl' style={{ textAlign: 'center' }}>
+          <h3
+            className='about-intro-h3 about-intro-urdu'
+            dir='rtl'
+            style={{ textAlign: 'center' }}>
             السلام علیکم
           </h3>
           <h1 className='about-intro-h2' style={{ textAlign: 'center' }}>
-            I am Kashaan
+            I am Kashaan Mahmood
           </h1>
-          <h2 className='about-intro-h3' style={{ textAlign: 'center' }}>
-            a Full Stack JavaScript Developer
-          </h2>
         </div>
         <div className='about-modal'>
           <button
